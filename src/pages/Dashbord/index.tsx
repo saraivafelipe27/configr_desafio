f35d5611b7e878ae2aee4d3cc751d7ad0109cdf0
saraivafelipe27 <saraivafelipe27@gmail.com>
2020-10-07 22:36:51 -0300

    update code

--- src/pages/Dashbord/index.tsx
+++ src/pages/Dashbord/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Title } from './styles'
 
 const Dashboard: React.FC = () => {
-  return <Title>Repositories</Title>;
+  return <Title>Issues</Title>;
 };
 
 export default Dashboard;
