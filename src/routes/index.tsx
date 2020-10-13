336976a490e31482403c87856984ccaa3783140d
saraivafelipe27 <saraivafelipe27@gmail.com>
2020-10-13 17:29:37 -0300

    update code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
-import IssuesC from '../pages/IssuesClosed';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
-    <Route path="/IssuesC" exact component={IssuesC} />
 
   </Switch>
 )
