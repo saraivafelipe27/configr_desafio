60f9bc0933fe1100cee3b8bd97fdcf2cf0a76921
saraivafelipe27 <saraivafelipe27@gmail.com>
2020-10-13 14:56:40 -0300

    update code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiChevronRight } from 'react-icons/fi';
 
 
 import api from '../../services/api';
